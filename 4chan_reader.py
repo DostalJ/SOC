009a6210e2f7a3d5fcc7bb6c72742de45accef46
Jakub Dostal <jakub@Jakubs-MacBook-Air.local>
2016-12-23 22:44:03 +0100

    Delete unnecessary files

--- 4chan_reader.py
+++
@@ -1,9 +0,0 @@
-import basc_py4chan
-# http://basc-py4chan.readthedocs.io/en/latest/index.html
-
-board = basc_py4chan.Board('r')
-thread_ids = board.get_all_thread_ids()
-thread = board.get_thread(thread_id=thread_ids[5])
-for post in thread.posts:
-    print(post.text_comment)
-    print('-'*60)
