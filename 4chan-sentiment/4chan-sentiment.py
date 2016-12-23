f831cbe4ef8b1dcc2c60fe258be1da637f76ddd6
Jakub Dostal <jakub@Jakubs-MacBook-Air.local>
2016-12-23 22:02:05 +0100

    Better 4chan-sentiment

--- 4chan-sentiment/4chan-sentiment.py
+++ 4chan-sentiment/4chan-sentiment.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 from time import sleep
 
 
-keywords = ['islam', 'isis', 'islamic state', 'muslim', 'terrorist', 'refugee', 'refugees']
+keywords = ['islam', 'isis', 'islamic state', 'muslim', 'muslims', 'terrorist', 'refugee', 'refugees']
 
 for i in range(10):
     sentiment = 0
@@ -12,11 +12,11 @@ for i in range(10):
     board = basc_py4chan.Board('news')
     all_threads = board.get_all_threads()
     for thread in all_threads:
-        if any(word in thread.topic.subject for word in keywords) or any(word in thread.topic.text_comment for word in keywords):
-            sentiment = (sentiment + TextBlob(thread.topic.subject + thread.topic.text_comment).sentiment.polarity)/2
+        if any(word in thread.topic.text_comment.lower() for word in keywords):
+            sentiment = (sentiment + TextBlob(thread.topic.text_comment.lower()).sentiment.polarity)/2
             num_of_threads += 1
             for post in thread.posts:
-                sentiment = (sentiment + TextBlob(post.text_comment).sentiment.polarity)/2
+                sentiment = (sentiment + TextBlob(post.text_comment.lower()).sentiment.polarity)/2
     print('Step:', i)
     print('Threads:', num_of_threads)
     print('Sentiment: {:.5f}'.format(sentiment))
