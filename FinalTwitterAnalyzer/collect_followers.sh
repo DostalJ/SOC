fc58e575d99164c8a703953e17332cb58850c84f
Jakub Dostal <jakub@Jakubs-MacBook-Air.local>
2017-03-09 06:04:10 +0100

    new data scrapped

--- FinalTwitterAnalyzer/collect_followers.sh
+++ FinalTwitterAnalyzer/collect_followers.sh
@@ -1,11 +1,11 @@
-python3 collect_followers.py -p=PetroleumEcon -n=100 -o=./measurements/people_to_follow/PetroleumEcon.txt
-
-python3 collect_followers.py -p=OnlyMormons -n=100 -o=./measurements/people_to_follow/OnlyMormons.txt
-
-python3 collect_followers.py -p=CatholicNewsSvc -n=100 -o=./measurements/people_to_follow/CatholicNewsSvc.txt
-
-python3 collect_followers.py -p=EvrydayFeminism -n=100 -o=./measurements/people_to_follow/EvrydayFeminism.txt
-
-python3 collect_followers.py -p=LGBTfdn -n=100 -o=./measurements/people_to_follow/LGBTfdn.txt
-
-python3 collect_followers.py -p=BiochemSoc -n=100 -o=./measurements/people_to_follow/BiochemSoc.txt
+# python3 collect_followers.py -p=PetroleumEcon -n=100 -o=./measurements/people_to_follow/PetroleumEcon.txt
+#
+# python3 collect_followers.py -p=OnlyMormons -n=100 -o=./measurements/people_to_follow/OnlyMormons.txt
+#
+# python3 collect_followers.py -p=CatholicNewsSvc -n=100 -o=./measurements/people_to_follow/CatholicNewsSvc.txt
+#
+# python3 collect_followers.py -p=EvrydayFeminism -n=100 -o=./measurements/people_to_follow/EvrydayFeminism.txt
+#
+# python3 collect_followers.py -p=LGBTfdn -n=100 -o=./measurements/people_to_follow/LGBTfdn.txt
+#
+# python3 collect_followers.py -p=BiochemSoc -n=100 -o=./measurements/people_to_follow/BiochemSoc.txt
--- FinalTwitterAnalyzer/process_data.py
+++ FinalTwitterAnalyzer/process_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.style import use
 use('ggplot')
+import pandas as pd
 
 f1 = open('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/BiochemSoc-trump.txt', 'r')
 sent1 = f1.read().split(',')[:-1]
@@ -10,9 +11,15 @@ f2 = open('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentimen
 sent2 = f2.read().split(',')[:-2]
 sent2 = [float(s) for s in sent2]
 
+df1 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/CatholicNewsSVC-abortion.txt', )
+df2 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/OnlyMormons-abortion.txt', )
 
-np.mean(sent1)
-np.mean(sent2)
+
+np.mean(df1['sent'])
+np.mean(df2['sent'])
+
+plt.hist([df1['sent'], df2['sent']], bins=30, normed=True)
+plt.show()
 
 plt.hist([sent1, sent2], bins=30, normed=True)
 plt.show()
