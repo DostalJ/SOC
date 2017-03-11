60377c3d801b43210803f9320976e4ad3daa7d12
Jakub Dostal <jakub@Jakubs-MacBook-Air.local>
2017-03-11 19:23:41 +0100

    data, abstract

--- FinalTwitterAnalyzer/process_data.py
+++ FinalTwitterAnalyzer/process_data.py
@@ -11,15 +11,18 @@ f2 = open('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentimen
 sent2 = f2.read().split(',')[:-2]
 sent2 = [float(s) for s in sent2]
 
-df1 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/CatholicNewsSVC-abortion.txt', )
-df2 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/OnlyMormons-abortion.txt', )
+df1 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/CatholicNewsSVC-abortion.txt', header=None)
+df2 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/OnlyMormons-abortion.txt', header=None)
+df3 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/EvryDayFeminism-abortion.txt', header=None)
+df4 = pd.read_csv('/Users/Jakub/MEGA/Work/SOC/FinalTwitterAnalyzer/measurements/sentiment/LGBTfdn-abortion.txt', header=None)
 
+dfs = [df1, df2, df3, df4]
+for df in dfs:
+    df.columns = ['date', 'sent']
 
-np.mean(df1['sent'])
-np.mean(df2['sent'])
-
-plt.hist([df1['sent'], df2['sent']], bins=30, normed=True)
+# plt.hist([df['sent'] for df in dfs], bins=15, normed=False, color=['b', 'r', 'g', 'k'])
+plt.hist([df1['sent'], df3['sent']], bins=15, normed=False, color=['b','g'])
 plt.show()
 
-plt.hist([sent1, sent2], bins=30, normed=True)
+plt.hist([sent1, sent2], bins=30, normed=False, color=['b', 'r'])
 plt.show()
